1c69690c6bde432763388c2c20d916baaecb1c64
GulchinTaghizade <1pylr6h@code.edu.az>
2022-06-16 00:25:11 +0400

    12:25

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+$(document).on('ready', function () {
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
